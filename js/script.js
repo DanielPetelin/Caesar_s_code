709e1b412d8b755ac393c18ad6d4974433276f84
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-28 10:17:16 +0300

    a

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 document.getElementById('in').oninput = function() {
     var help = document.getElementById('help');
+    help.innerHTML = String.fromCharCode(97);
+    var str = this.value;
+    console.log(str.charCodeAt(0));
 }
